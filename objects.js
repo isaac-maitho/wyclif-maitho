5ec62e4c21baecac7b7cae75a26c01ca640a22a7
Isaac Maitho <110528687+wyclif-maitho@users.noreply.github.com>
2023-01-03 12:54:11 +0300

    Add files via upload

---
+++ objects.js
@@ -0,0 +1,11 @@
+let retreatMessage = 'I am a SW engineer'
+const alienShip ={
+  retreat() {
+    console.log(retreatMessage)
+  },
+  takeOff() {
+    console.log('Spim... Borp... Glix... Blastoff!')
+  }
+};
+alienShip.retreat();
+alienShip.takeOff();
